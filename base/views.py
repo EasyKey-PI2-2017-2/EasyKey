08b3fcc38d6d20faf953fb366c70e4e3a82a151b
Luciano Almeida <mdiebr@gmail.com>
2017-08-27 15:16:33 -0300

    Create app base
    Signed-off-by: Luciano Almeida <mdiebr@gmail.com>

---
+++ base/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Home")
+
+
+def login(request):
+    return HttpResponse("Login")
+
+
+def signup(request):
+    return HttpResponse("Cadastro")
