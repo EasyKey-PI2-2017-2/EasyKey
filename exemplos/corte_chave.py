a38d14050fef7b8292dc16667c9a6070ec14ba61
Luciano Almeida <mdiebr@gmail.com>
2017-09-13 17:59:21 -0300

    Mudanças no exemplo e foto com dilatação/erosão
    Signed-off-by: Luciano Almeida <mdiebr@gmail.com>

--- exemplos/corte_chave.py
+++ exemplos/corte_chave.py
@@ -9,12 +9,16 @@ def load():
     os.system('convert a2.jpg -crop 150x315+185+118 +repage cp.jpg')
     img = cv2.imread('cp.jpg')
     imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(imgray, (5,5), 1)
-    ret,thresh = cv2.threshold(blur,127,255,0)
+    blur = cv2.GaussianBlur(imgray, (5,5), 0)
+
+    ret,t = cv2.threshold(blur,127,255,0)
+    thresh = cv2.erode(t, None, iterations=1)
+
     image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,
                                                   cv2.CHAIN_APPROX_SIMPLE)
-    return cv2.drawContours(img, contours, -1, (255,255,255), 1), im
+    return (cv2.drawContours(img, contours, -1, (255,255,255), 1), im,
+            contours, t, thresh)
 
 if __name__ == '__main__':
-    img, im = load()
+    img, im, cnt, o, t = load()
 
