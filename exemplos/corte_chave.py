7c8032867025f7cce810014c66ca974ff0d1f301
Luciano Almeida <mdiebr@gmail.com>
2017-09-22 17:15:11 -0300

    Gera GCode da imagem de exemplo
    Signed-off-by: Luciano Almeida <mdiebr@gmail.com>

--- exemplos/corte_chave.py
+++ exemplos/corte_chave.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 import subprocess
 import os
 
+from math import pow, sqrt
+
+# escala de 1 mm/px
+ESCALA = 0.5
+
 def load():
     im = cv2.imread('a2.jpg')
     os.system('convert a2.jpg -crop 75x315+185+118 +repage cp.jpg')
@@ -16,13 +21,45 @@ def load():
 
     return (t_antes, t_depois)
 
+
+def gcode(img):
+    f = open('gcode.nc', 'w')
+    f.write(g0(0, -2))
+
+    for x, linha in enumerate(img):
+        for y, pixel in enumerate(linha):
+            if pixel == 0:
+                referencia = (x, y)
+                break
+        if pixel == 0:
+            break
+
+    for x, linha in enumerate(img):
+        for y, pixel in enumerate(linha):
+            if pixel == 0:
+                f.write(g1(referencia, (x,y)))
+                break
+
+    f.write('M2')
+
+
+# define a posição inicial
+def g0(x, y):
+    return 'G0 X{} Y{}\n'.format(x, y)
+
+# movimentação do corte
+def g1(referencia, pixel):
+    difx = referencia[0] - pixel[0]
+    dify = referencia[1] - pixel[1]
+
+    return 'G1 X{} Y{}\n'.format(difx * ESCALA , dify * ESCALA)
+
 if __name__ == '__main__':
     t_antes, t_depois = load()
     final = (t_antes - t_depois)
     final = (255-final)
     cv2.imwrite('dilate-erode.jpg', final)
-    
+
     os.system('convert dilate-erode.jpg dilate-erode.pnm')
     os.system('mkbitmap dilate-erode.pnm -f 2 -s 2 -t 0.48 -o dilate-erode2.pnm')
     os.system('potrace dilate-erode2.pnm -s -t 5 -o dilate-erode.svg')
-    
