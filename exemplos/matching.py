0e3e5abdb2e20c81b729e858ea2bb4dec71e000b
Luis Henrique <luishng123@gmail.com>
2017-09-27 10:51:03 -0300

    Adicinando imagens no codigo de matching

--- exemplos/matching.py
+++ exemplos/matching.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy as np
 
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+img = cv2.imread('a2.jpg')
+template = cv2.imread('pado_cabeca.jpg',0)
+# template = cv2.imread('formatch.jpg',0)
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 w, h = template.shape[::-1]
 res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
 threshold = 0.85
