8c7a8d737ff02457e9828de69a18dfb824239ea8
Luis HEnrique <luishng123@gmail.com>
2017-11-16 10:50:40 -0200

    Primeira refatoração do código

--- base/models.py
+++ base/models.py
@@ -1,3 +1,105 @@
 from django.db import models
+import numpy as np
+import cv2
+import glob
 
-# Create your models here.
+
+class Key():
+    def __init__(self):
+        self.key = 0
+        self.templates = []
+        self.match = 0
+        self.contour = 0
+        self.scale = 0
+
+    def load_key(self):
+        # TODO Alterar quando estiver com a estrutura pronta
+        # TODO Tirar a foto usando o PiCamera e salvar nesse path abaixo
+        # img = cv2.imread('media/chave.jpg')
+        img = cv2.imread('media/a2.jpg')
+        cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        self.key = cinza
+
+    def load_templates(self):
+        modelos = glob.glob('media/templates/*.jpg')
+        for path in modelos:
+            self.templates.append(path)
+
+    def verify_key_model(self):
+        match = False
+        for template in self.templates:
+            img_template = cv2.imread(template, 0)
+            w, h = img_template.shape[::-1]
+            res = cv2.matchTemplate(self.key, img_template, cv2.TM_CCOEFF_NORMED)
+            threshold = 0.85
+            loc = np.where(res >= threshold)
+            if len(loc[0]) > 0:
+                match = True
+                break
+            else:
+                match = False
+        return match
+
+    def define_contour(self):
+        # transformações para imagem da chave
+        # TODO verificar size da chave pra cortar do size certo
+        key = self.key[114:433, 190:260]
+        borrado = cv2.GaussianBlur(key, (5, 5), 0)
+        _, key_limite_antes = cv2.threshold(borrado, 127, 255, 0)
+        key_limite_depois = cv2.erode(key_limite_antes, None, iterations=1)
+        key_contour = (key_limite_antes - key_limite_depois)
+        key_final = (255 - key_contour)
+        self.contour = key_final
+
+    def define_scale(self):
+        # TODO quando tivermos a scale definitiva, descomentamos isso daqui
+        # transformações para imagem da scale
+        #scale = self.key[140:190, 45:55]
+        #_, scale_limite = cv2.threshold(scale, 127, 255, 0)
+        #scale_inversa = (255 - scale_limite)
+        #scale_final = scale_inversa.transpose()
+
+        # TODO retirar isso quando tivermos scale
+        return 1
+
+        first = 0
+        last = 0
+        for y, row in enumerate(scale_final):
+            for x, pixel in enumerate(row):
+                if pixel == 255 and first == 0:
+                    first = x
+                elif pixel == 255 and x < first:
+                    first = x
+                elif pixel == 255 and x > last:
+                    last = x
+        size = last-first
+
+        # esse 1 no retorno é 1cm, valor conhecido de referência
+        self.scale = 1/size
+
+    def gcode(self):
+        f = open('media/gcode.nc', 'w')
+        f.write(self.g0(0, -2))
+
+        for x, row in enumerate(self.contour):
+            for y, pixel in enumerate(row):
+                if pixel == 0:
+                    reference= (x, y)
+                    break
+            if pixel == 0:
+                break
+        for x, row in enumerate(self.contour):
+            for y, pixel in enumerate(row):
+                if pixel == 0:
+                    f.write(self.g1(reference, (x,y), self.scale))
+                    break
+        f.write('M2')
+        f.close()
+
+    def g0(self, x, y):
+        return 'G0 X{} Y{}\n'.format(x, y)
+
+    def g1(self, reference, pixel, scale):
+        difx = reference[0] - pixel[0]
+        dify = reference[1] - pixel[1]
+        return 'G1 X{} Y{}\n'.format(difx * scale , dify * scale)
--- base/views.py
+++ base/views.py
@@ -1,13 +1,11 @@
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 import datetime
 
-from base.forms import SignupForm
-from chave.views import Chave
+from .models import Key
+
 
 def home(request):
     if request.user.is_authenticated():
@@ -15,28 +13,28 @@ def home(request):
     else:
         hour = datetime.datetime.now().strftime('%H');
         hour = int(hour)
-        if hour >= 0 and hour <= 11:
-            mensagem = "Bom Dia!"
-        elif hour >= 12 and hour <= 17:
-            mensagem = "Boa Tarde!"
-        else:
-            mensagem = "Boa Noite!"
-        return render(request, 'copy/home.html', {'mensagem': mensagem})
+        message = hello_msg(hour)
+
+        return render(request, 'copy/home.html', {'message': message})
+
 
 @login_required(login_url='login')
 def key_code(request):
     if request.method == 'POST':
-        chave = Chave()
-        chave.carregar_chave()
-        chave.carregar_templates()
-        match = chave.verificar_modelo()
+        key = Key()
+        key.load_key()
+        key.load_templates()
+        match = key.verify_key_model()
+        
         if match:
-            chave.definir_escala()
-            chave.definir_contorno()
-            chave.gcode()
+            key.define_contour()
+            key.define_scale()
+            key.gcode()
+            
             return redirect('key_cut')
         else:
             error = True
+            
             return render(request, 'copy/key_code.html', {'error': error})
     else:
         error = request.GET.get('error');
@@ -47,29 +45,26 @@ def key_code(request):
             error = True
             return render(request, 'copy/key_code.html', {'error2': error})
 
+
 @login_required(login_url='login')
 def key_cut(request):
     return render(request, 'copy/key_cut.html')
 
+
 @login_required(login_url='login')
 def key_finish(request):
     return render(request, 'copy/key_finish.html')
+
 
 def signup(request):
     if request.method == 'POST':
         name = request.POST['first_name']
         username = request.POST['username']
         password = request.POST['password1']
-        passwordVerify = request.POST['password2']
-        error = "";
-        if passwordVerify != password:
-            error = "- As senhas não conferem "
-        if len(password) < 8:
-            error = error + "- A senha não tem 8 caracteres "
-        if len(username) < 1:
-            error = error + "- Digite o usuário"
-        if password.isdigit():
-            error = error + "- A senha possui apenas digitos"
+        password_verify = request.POST['password2']
+        
+        error = validate_signup(username, password, password_verify);
+        
         if len(error) == 0:
             if not User.objects.filter(username = username).exists():
                 user = User.objects.create_user(username, name, password)
@@ -94,21 +89,54 @@ def signup(request):
     else:
         return render(request, 'base/signup.html')
 
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        
         user = authenticate(username=username, password=password)
+        
         if user is not None:
             login(request, user)
+
             return redirect('home')
         else:
             error = True
+
             return render(request, 'base/login.html', {'error': error})
     else:
         return render(request, 'base/login.html')
 
+
 @login_required(login_url='login')
 def logout_view(request):
     logout(request)
+    
     return redirect('home')
+
+
+def hello_msg(hour):
+    if hour >= 0 and hour <= 11:
+        message = "Bom Dia!"
+    elif hour >= 12 and hour <= 17:
+        message = "Boa Tarde!"
+    else:
+        message = "Boa Noite!"
+
+    return message
+
+
+def validate_signup(username, password, password_verify):
+    error = ""
+    
+    if password_verify != password:
+        error = "- As senhas não conferem "
+    if len(password) < 8:
+        error = error + "- A senha não tem 8 caracteres "
+    if len(username) < 3:
+        error = error + "- Digite o usuário maior que 3 caracteres "
+    if password.isdigit():
+        error = error + "- A senha possui apenas dígitos "
+        
+    return error
