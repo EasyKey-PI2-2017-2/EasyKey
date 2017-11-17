09338d9badd705fbfd74f46b4da60ad8668968c5
Luis HEnrique <luishng123@gmail.com>
2017-11-17 09:12:45 -0200

    Removendo funções do login

--- base/views.py
+++ base/views.py
@@ -1,7 +1,4 @@
-from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from paypal.standard.forms import PayPalPaymentsForm
 import datetime
@@ -22,7 +19,6 @@ def home(request):
         return render(request, 'copy/home.html', {'message': message})
 
 
-@login_required(login_url='login')
 def key_code(request):
     if request.method == 'POST':
         key = Key()
@@ -50,14 +46,12 @@ def key_code(request):
             return render(request, 'copy/key_code.html', {'error2': error})
 
 
-@login_required(login_url='login')
 def key_cut(request):
     key = Key()
     key.enviar_comandos()
     return render(request, 'copy/key_cut.html')
 
 
-@login_required(login_url='login')
 def key_payment(request):
     # What you want tha button to do.
     ale = random.randint(1, 100)
@@ -80,75 +74,9 @@ def key_payment(request):
 
     return render(request, "copy/key_payment.html", context)
 
-@login_required(login_url='login')
+
 def key_finish(request):
     return render(request, 'copy/key_finish.html')
-
-
-def signup(request):
-    if request.method == 'POST':
-        name = request.POST['first_name']
-        username = request.POST['username']
-        password = request.POST['password1']
-        password_verify = request.POST['password2']
-        
-        error = validate_signup(username, password, password_verify);
-        
-        if len(error) == 0:
-            if not User.objects.filter(username = username).exists():
-                user = User.objects.create_user(username, name, password)
-                user.save()
-
-                login(request, user)
-
-                return redirect('home')
-            else:
-                error_bool = True
-                error = "Já existe uma conta com esse usuário!"
-
-                information = {
-                    'error_bool': error_bool,
-                    'error': error,
-                }
-
-                return render(request, 'base/signup.html', information)
-        else:
-            error_bool = True
-
-            information = {
-                'error_bool': error_bool,
-                'error': error,
-            }
-
-            return render(request, 'base/signup.html', information)
-    else:
-        return render(request, 'base/signup.html')
-
-
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        
-        user = authenticate(username=username, password=password)
-        
-        if user is not None:
-            login(request, user)
-
-            return redirect('home')
-        else:
-            error = True
-
-            return render(request, 'base/login.html', {'error': error})
-    else:
-        return render(request, 'base/login.html')
-
-
-@login_required(login_url='login')
-def logout_view(request):
-    logout(request)
-    
-    return redirect('home')
 
 
 def hello_msg(hour):
@@ -160,19 +88,3 @@ def hello_msg(hour):
         message = "Boa Noite!"
 
     return message
-
-
-def validate_signup(username, password, password_verify):
-    error = ""
-    
-    if password_verify != password:
-        error = "- As senhas não conferem "
-    if len(password) < 8:
-        error = error + "- A senha não tem 8 caracteres "
-    if len(username) < 3:
-        error = error + "- Digite o usuário maior que 3 caracteres "
-    if password.isdigit():
-        error = error + "- A senha possui apenas dígitos "
-        
-    return error
-
